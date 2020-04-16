4e3595756457c8152c84c7f7a2d107b431ee28aa
Nicolas Martin <nicolasfelipemartin@hotmail.com>
2020-04-16 11:00:58 -0500

    Se agregan componentes que podriamos requerir en el momento y archivo de rutas como tambien carpeteado de modelos y servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Rutas
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
+
+
+
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProfilesComponent } from './components/profiles/profiles.component';
+import { AdminFilmsComponent } from './components/admin-films/admin-films.component';
+import { AddFilmComponent } from './components/add-film/add-film.component';
+import { UserRegisterComponent } from './components/user-register/user-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    HomeComponent,
+    FooterComponent,
+    ProfilesComponent,
+    AdminFilmsComponent,
+    AddFilmComponent,
+    UserRegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
