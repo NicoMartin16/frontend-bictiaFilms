43172545bbaa8ca80b596cacc98756db6107130d
chrisdev12 <christianm.bravop@gmail.com>
2020-04-19 14:21:14 -0500

    Integrando estilos continuos ||Pendiente implementación del endpoint de addFilm

--- src/app/components/add-film/add-film.component.ts
+++ src/app/components/add-film/add-film.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Film } from "../../models/film";
+import { Form } from "@angular/forms";
+import { FilmService } from "../../services/film.service";
 
 @Component({
-  selector: 'app-add-film',
-  templateUrl: './add-film.component.html',
-  styleUrls: ['./add-film.component.css']
+    selector: "app-add-film",
+    templateUrl: "./add-film.component.html",
+    styleUrls: ["./add-film.component.css"],
 })
 export class AddFilmComponent implements OnInit {
+    public film: Film;
 
-  constructor() { }
+    constructor() {
+        this.film = new Film();
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
 
+    createFilm() {
+    }
 }
